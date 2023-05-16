526195588c070565bdccbeb987b76f42595d2f91
Thiago Soares Mota <thiagomota456@gmail.com>
2023-05-16 14:29:25 -0300

    Add Cadastrar clientes(Estrutura/front): Função para cadastrar informações dos clientes, como nome, endereço, telefone, e-mail, etc.#2

--- form/script/script.js
+++ form/script/script.js
@@ -1,51 +1,69 @@
 var producs = ["Finalizar Venda", "Baby Doll de Setim", "Baby Doll Signois", "Body Corpeti", "Baby Doll Flu Flu", "Baby Doll Kids", "Baby Doll Renda Monica", "Baby Doll Times", "Body Tule Sabrina", "Bodys Claire s/ Bojo", "Bodys Tule Sabrina Renda", "Calcinha Callesson", "Calcinha Pluze Sine", "Camisola de Renda Personagens", "Camisola Ultra Luxo Milena", "Coj. s/ Bojo Lisa", "Coj. Ultra Luxo Julia", "Coj. Sem Costura", "Conjunto Babartana Luxo", "Conjunto Chic Luxo c/ Perneira", "Conjunto Dia a Dia", "Conjunto Ultra Luxo Bordado", "Conjunto Cropped Laura", "Conjunto Camisola Ultra Luxo Diana", "Conjunto s/ Bojo Aro Luiza", "Espartilho Mimmus", "Kit Casal", "Kit Levanta Seios", "Kit Mãe e Filha", "Kit Pijama Estampado c/ Tapaoelho", "Macaquinho", "Pijama Calça Comprida", "Pijama de Botão", "Pijama Pluze Size Americano", "Sabonetes Íntimos", "Sutiã Malu Reforçado"];
 var options = ["PP", "P", "M", "G", "XG"];
 
-function saveFormData() {
-    var formData = {
-      nome: document.getElementById("nome").value,
-      endereco: document.getElementById("endereco").value,
-      telefone: document.getElementById("telefone").value,
-      email: document.getElementById("email").value,
-      option1: document.getElementById("option1").checked,
-      option2: document.getElementById("option2").checked,
-      option3: document.getElementById("option3").checked
-    };
-  
-    localStorage.setItem("formData", JSON.stringify(formData));
-    window.location.href = "formpt2.html";
-  }
-function showProgressBar() {
+var nomes = [
+  "Ana Silva",
+  "Pedro Santos",
+  "Sofia Oliveira",
+  "Luís Pereira",
+  "Carolina Costa",
+  "Miguel Ferreira",
+  "Beatriz Almeida",
+  "Guilherme Rodrigues",
+  "Mariana Neves",
+  "Tiago Gomes"
+];
 
-  var progressBar = document.getElementById("progressBar");
-  progressBar.innerHTML = '<div class="progress-bar-fill"></div>';
-}
+var enderecos = [
+  "Rua das Flores, 123 - Centro, São Paulo - SP, 01001-000",
+  "Avenida Paulista, 456 - Bela Vista, São Paulo - SP, 01310-100",
+  "Rua do Rosário, 789 - Santa Efigênia, Rio de Janeiro - RJ, 20041-002",
+  "Praça da Liberdade, 246 - Funcionários, Belo Horizonte - MG, 30140-010",
+  "Rua da Consolação, 1350 - Consolação, São Paulo - SP, 01301-100",
+  "Rua do Acre, 234 - Centro, Rio de Janeiro - RJ, 20081-240",
+  "Avenida Atlântica, 1702 - Copacabana, Rio de Janeiro - RJ, 22021-000",
+  "Rua 24 de Maio, 105 - República, São Paulo - SP, 01041-001",
+  "Rua do Lavradio, 132 - Lapa, Rio de Janeiro - RJ, 20230-070",
+  "Rua da Praia, 789 - Centro Histórico, Porto Alegre - RS, 90020-307"
+];
 
-window.onload = function() {
-  var formData = localStorage.getItem("formData");
+var telefones = [
+  "(11) 98765-4321",
+  "(21) 91234-5678",
+  "(31) 92345-6789",
+  "(41) 93456-7890",
+  "(51) 94567-8901",
+  "(61) 95678-9012",
+  "(71) 96789-0123",
+  "(81) 97890-1234",
+  "(91) 98901-2345",
+  "(48) 99012-3456",
+  "+1 (123) 456-7890",
+  "123-456-7890",
+  "(123) 456 7890",
+  "1234567890"
+];
 
-  if (formData) {
-    formData = JSON.parse(formData);
-    var formResults = document.getElementById("formResults");
+var emails = [
+  "ana_silva@example.com",
+  "pedro_santos@example.com",
+  "sofia_oliveira@example.com",
+  "luis_pereira@example.com",
+  "carolina_costa@example.com",
+  "miguel_ferreira@example.com",
+  "beatriz_almeida@example.com",
+  "guilherme_rodrigues@example.com",
+  "mariana_neves@example.com",
+  "tiago_gomes@example.com"
+];
 
-    formResults.innerHTML = "<h2>Dados do Formulário</h2>";
-    formResults.innerHTML += "<p>Nome: " + formData.nome + "</p>";
-    formResults.innerHTML += "<p>Endereço: " + formData.endereco + "</p>";
-    formResults.innerHTML += "<p>Telefone: " + formData.telefone + "</p>";
-    formResults.innerHTML += "<p>E-mail: " + formData.email + "</p>";
-    formResults.innerHTML += "<p>Option 1: " + (formData.option1 ? "Selecionado" : "Não selecionado") + "</p>";
-    formResults.innerHTML += "<p>Option 2: " + (formData.option2 ? "Selecionado" : "Não selecionado") + "</p>";
-    formResults.innerHTML += "<p>Option 3: " + (formData.option3 ? "Selecionado" : "Não selecionado") + "</p>";
 
-    localStorage.removeItem("formData");
-  }
-};
 
 function buscarProdutos() {
 
   const inputProduto = document.getElementById('produto');
   const valorProduto = inputProduto.value.toLowerCase();
-  const listaProdutos = producs;;
+  const listaProdutos = producs;
   const resultado = document.getElementById('resultado');
   resultado.innerHTML = '';
 
@@ -104,7 +122,7 @@ function adicionarOpcoesProduto() {
   divOpcoesMaca.appendChild(selectTipoDeProduto);
   divOpcoesMaca.appendChild(document.createElement('br'));
   divOpcoesMaca.appendChild(labelQuantidadeProduto);
-  divOpcoesMaca.appendChild(inputQuantidadeProduto);
+  divOpcoesMaca.appendChild(inputQuantidadeProduto);c
 
   const formContainer = document.querySelector('#moreProduct');
   formContainer.appendChild(divOpcoesMaca);
@@ -117,4 +135,176 @@ function enviar(){
     var tamanho = document.getElementById("tipo-produto-"+i).value;
     var quantidade = document.getElementById("quantidade-produto-"+i).value;
   }
+}
+
+function buscarTelefone() {
+  const inputTelefone = document.getElementById('telefone');
+  const valorTelefone = inputTelefone.value;
+  const listaDeNumerosCadastrados = numeros; // substitua "numeros" pela sua lista de números cadastrados
+  const resultado = document.getElementById('resultado-telefone');
+  resultado.innerHTML = '';
+
+  listaDeNumerosCadastrados.forEach((numero) => {
+    if (numero.includes(valorTelefone)) {
+      const listItem = document.createElement('li');
+      listItem.textContent = numero;
+      listItem.addEventListener('click', () => {
+        inputTelefone.value = numero;
+        resultado.innerHTML = '';
+      });
+      resultado.appendChild(listItem);
+    }
+  });
+}
+
+function buscarEndereco(){
+
+  const inputProduto = document.getElementById('endereco');
+  const valorProduto = inputProduto.value.toLowerCase();
+  const listaDenomesCadatrados = enderecos;
+  const resultado = document.getElementById('resultado-endereco');
+  resultado.innerHTML = '';
+
+  listaDenomesCadatrados.forEach((nome) => {
+    if (nome.toLowerCase().includes(valorProduto)) {
+      const listItem = document.createElement('li');
+      listItem.textContent = nome;
+      listItem.addEventListener('click', () => {
+        inputProduto.value = nome;
+        resultado.innerHTML = '';
+      });
+      resultado.appendChild(listItem);
+    }
+  });
+}
+
+function buscarTelefone(){
+
+  const inputProduto = document.getElementById('telefone');
+  const valorProduto = inputProduto.value;
+  const listaDenomesCadatrados = telefones;
+  const resultado = document.getElementById('resultado-telefone');
+  resultado.innerHTML = '';
+
+  listaDenomesCadatrados.forEach((nome) => {
+    if (nome.includes(valorProduto)) {
+      const listItem = document.createElement('li');
+      listItem.textContent = nome;
+      listItem.addEventListener('click', () => {
+        inputProduto.value = nome;
+        resultado.innerHTML = '';
+      });
+      resultado.appendChild(listItem);
+    }
+  });
+
+}
+
+function buscarEmail(){
+
+  const inputProduto = document.getElementById('email');
+  const valorProduto = inputProduto.value.toLowerCase();
+  const listaDenomesCadatrados = emails;
+  const resultado = document.getElementById('resultado-email');
+  resultado.innerHTML = '';
+
+  listaDenomesCadatrados.forEach((nome) => {
+    if (nome.toLowerCase().includes(valorProduto)) {
+      const listItem = document.createElement('li');
+      listItem.textContent = nome;
+      listItem.addEventListener('click', () => {
+        inputProduto.value = nome;
+        resultado.innerHTML = '';
+      });
+      resultado.appendChild(listItem);
+    }
+  });
+}
+
+function buscarNome(){
+
+  const inputProduto = document.getElementById('nome');
+  const valorProduto = inputProduto.value.toLowerCase();
+  const listaDenomesCadatrados = nomes;
+  const resultado = document.getElementById('resultado-nome');
+  resultado.innerHTML = '';
+
+  listaDenomesCadatrados.forEach((nome) => {
+    if (nome.toLowerCase().includes(valorProduto)) {
+      const listItem = document.createElement('li');
+      listItem.textContent = nome;
+      listItem.addEventListener('click', () => {
+        inputProduto.value = nome;
+        resultado.innerHTML = '';
+      });
+      resultado.appendChild(listItem);
+    }
+  });
+}
+
+
+// Função para calcular a distância de edição entre duas strings
+function calculateEditDistance(a, b) {
+  if (a.length === 0) return b.length;
+  if (b.length === 0) return a.length;
+
+  const matrix = [];
+
+  for (let i = 0; i <= b.length; i++) {
+    matrix[i] = [i];
+  }
+
+  for (let j = 0; j <= a.length; j++) {
+    matrix[0][j] = j;
+  }
+
+  for (let i = 1; i <= b.length; i++) {
+    for (let j = 1; j <= a.length; j++) {
+      if (b.charAt(i - 1) === a.charAt(j - 1)) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        matrix[i][j] = Math.min(
+          matrix[i - 1][j - 1] + 1, // Substituição
+          matrix[i][j - 1] + 1, // Inserção
+          matrix[i - 1][j] + 1 // Deleção
+        );
+      }
+    }
+  }
+
+  return matrix[b.length][a.length];
+}
+
+// Função para pesquisar a palavra/frase no array
+function searchInArray(query, array) {
+  const results = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i];
+    const distance = calculateEditDistance(query.toLowerCase(), item.toLowerCase());
+    const similarity = 1 - distance / Math.max(query.length, item.length);
+
+    if (similarity >= 0.8) { // Define um limite de similaridade mínima de 80%
+      results.push({ item, similarity });
+    }
+  }
+
+  return results;
+}
+
+function searchPhoneNumber(phoneNumber, phoneArray) {
+  // Remover caracteres especiais do número de telefone de pesquisa
+  const cleanPhoneNumber = phoneNumber.replace(/[\(\)\-\s+]/g, '');
+
+  // Percorrer o vetor de telefones
+  for (let i = 0; i < phoneArray.length; i++) {
+    const cleanPhone = phoneArray[i].replace(/[\(\)\-\s+]/g, '');
+
+    // Verificar se o número de telefone corresponde
+    if (cleanPhone === cleanPhoneNumber) {
+      return true;
+    }
+  }
+
+  return false;
 }
